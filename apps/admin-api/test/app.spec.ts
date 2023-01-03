a68cdd1ca1d020edaff0057ec58624e991ae7d40
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-03 19:19:30 +0630

    test(admin-api): add test to add new books
    app.spec.ts    : add test to add new books with sample test data
    bookData.json  : full list of data if needed

--- apps/admin-api/test/app.spec.ts
+++ apps/admin-api/test/app.spec.ts
@@ -2,6 +2,31 @@ import request from "supertest"
 import mongoose from "mongoose"
 import app from "../src/app/app"
 
+const testData = [
+    {
+        category: "Algorithms",
+        name: "Clean Code Handbook: 50 Common Interview Questions",
+        author: "LeetCode",
+        dateAdded: "2022:05:02 14:01:30+06:30",
+    },
+    {
+        category: "Biography",
+        name: "Amazon Unbound: Jeff Bezos and the Invention of a Global Empire",
+        author: "Brad Stone",
+        isbn: "9781982132613",
+        dateAdded: "2022:03:16 15:07:26+06:30",
+        olid: "OL32070424M",
+    },
+    {
+        category: "Fiction",
+        name: "The Count of Monte Cristo",
+        author: "Alexandre Dumas",
+        isbn: "9780140449266",
+        dateAdded: "2010:10:09 22:50:36+06:30",
+        olid: "OL24277771M",
+    },
+]
+
 // Connect to database before all tests
 beforeAll(async () => {
     if (!process.env.MONGO_URI) {
@@ -16,6 +41,7 @@ afterAll(async () => {
 })
 
 const userCount = Math.floor(Math.random() * 1000)
+let token: string
 
 describe("Books", () => {
     it("SignUp (POST /v1/signup)", async () => {
@@ -36,6 +62,15 @@ describe("Books", () => {
                 name: `user${userCount}`,
                 password: `user${userCount}`,
             })
+        token = res.body.token
         expect(res.statusCode).toBe(200)
+    })
+
+    it("Add Books (POST /v1/books)", async () => {
+        const res = await request(app)
+            .post("/v1/books")
+            .set("Authorization", "bearer " + token)
+            .send(testData)
+        expect(res.statusCode).toBe(201)
     })
 })

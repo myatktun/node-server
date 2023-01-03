b05f3422a7910c19cdc89c0e62fe50f124296332
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-03 19:22:14 +0630

    fix(admin-api): fix jest deprecation warnings
    jest.config.ts : fix globals is deprecated warning

--- apps/admin-api/jest.config.ts
+++ apps/admin-api/jest.config.ts
@@ -2,15 +2,12 @@
 export default {
     displayName: "admin-api",
     preset: "../../jest.preset.js",
-    globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/tsconfig.spec.json",
-        },
-    },
     testEnvironment: "node",
     transform: {
-        "^.+\\.[tj]s$": "ts-jest",
+        "^.+\\.[tj]s$": [
+            "ts-jest",
+            { tsconfig: "<rootDir>/tsconfig.spec.json" },
+        ],
     },
     moduleFileExtensions: ["ts", "js", "html"],
-    coverageDirectory: "../../coverage/apps/admin-api",
 }

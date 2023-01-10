a77528e2a10aedff6142e854a8ec3e18ec083d42
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-10 23:49:08 +0630

    test(user): add cypress e2e tests for user application
    Still an empty test

---
+++ apps/user-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from "../support/app.po"
+
+describe("user", () => {
+    beforeEach(() => cy.visit("/"))
+
+    it("should have header", () => {
+        // Custom command example, see `../support/commands.ts` file
+        cy.login("my-email@something.com", "myPassword")
+
+        // Function helper example, see `../support/app.po.ts` file
+        getGreeting().contains("Welcome to SharedUiHeader User")
+    })
+})

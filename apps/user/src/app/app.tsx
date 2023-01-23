898391e07a7714b83b13dc78ed95283d87126ea1
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-23 23:13:13 +0630

    feat(user): add react-query and carousel component
    src/app/app.tsx : wrap components with QueryClientProvider, add carousel
                      components based on number of items in titles array

--- apps/user/src/app/app.tsx
+++ apps/user/src/app/app.tsx
@@ -1,7 +1,19 @@
 import { SharedUiHeader } from "@projectx/shared/ui/header"
+import { SharedUiCarousel } from "@projectx/shared/ui/carousel"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient()
+const titles = ["Books", "Notes"]
 
 export function App() {
-    return <SharedUiHeader />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SharedUiHeader />
+            {titles.map((title) => (
+                <SharedUiCarousel title={title} />
+            ))}
+        </QueryClientProvider>
+    )
 }
 
 export default App

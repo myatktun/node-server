de2973541e8ca4f25cdca109522f6e33bf939d30
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-25 19:02:24 +0630

    refactor(user): rename components
    Rename components from "SharedUi<component>" to "<component>"

--- apps/user/src/app/app.tsx
+++ apps/user/src/app/app.tsx
@@ -1,4 +1,4 @@
-import { SharedUiHeader, SharedUiCarousel, SharedUiFooter } from "@projectx/shared/ui"
+import { Header, Carousel, Footer } from "@projectx/shared/ui"
 import { QueryClient, QueryClientProvider } from "react-query"
 
 const queryClient = new QueryClient()
@@ -7,11 +7,11 @@ const titles = ["Books", "Notes"]
 export function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <SharedUiHeader />
+            <Header />
             {titles.map((title, index) => (
-                <SharedUiCarousel key={index} title={title} index={index} />
+                <Carousel key={index} title={title} index={index} />
             ))}
-            <SharedUiFooter />
+            <Footer />
         </QueryClientProvider>
     )
 }

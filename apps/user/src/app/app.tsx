aa8f40b361a67a19dbc77c4f1e3a265a4261e188
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-27 06:51:31 +0630

    feat(user): add key and new routes
    app/app.tsx : add key to page component to reset state when route
                  change, add new routes to authors and categories

--- apps/user/src/app/app.tsx
+++ apps/user/src/app/app.tsx
@@ -5,13 +5,23 @@ import { Header, Home, Page, Footer } from "@projectx/shared/ui"
 const queryClient = new QueryClient()
 
 export function App() {
+    if (!process.env.NX_API_URL) {
+        console.error(new Error("No API URL set"))
+        return <div>Something went wrong</div>
+    }
+
     return (
         <QueryClientProvider client={queryClient}>
             <Header />
             <Routes>
                 <Route path="/" element={<Home titles={["Books", "Notes"]} />} />
-                <Route path="/books" element={<Page title={"books"} />} />
-                <Route path="/notes" element={<Page title={"notes"} />} />
+                <Route path="/books" element={<Page title={"books"} key="books" />} />
+                <Route path="/notes" element={<Page title={"notes"} key="notes" />} />
+                <Route path="/authors" element={<Page title={"authors"} key="authors" />} />
+                <Route
+                    path="/categories"
+                    element={<Page title={"categories"} key="categories" />}
+                />
             </Routes>
             <Footer />
         </QueryClientProvider>

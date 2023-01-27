1ebaefc45c7f9cfaf2755919b5d1c39fbc82b783
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-27 12:40:05 +0630

    perf(ui-home)!: query data based on titles props
    home.tsx : since carousel component only accept data array, query the
               data based on titles first and pass the result to carousel

--- libs/shared/ui/src/lib/home/home.tsx
+++ libs/shared/ui/src/lib/home/home.tsx
@@ -1,3 +1,4 @@
+import { useQueries } from "react-query"
 import { StyledHome } from "./home.styles"
 import { Carousel } from "../carousel/carousel"
 
@@ -6,10 +7,27 @@ interface HomeProps {
 }
 
 export function Home({ titles }: HomeProps) {
+    const fetchData = async (title: string) => {
+        const res = await fetch(`${process.env.NX_API_URL}/${title.toLowerCase()}?sort=dateAdded`)
+        return res.json()
+    }
+
+    const all = useQueries(
+        titles.map((title) => {
+            return {
+                queryKey: [title],
+                queryFn: () => fetchData(title),
+            }
+        })
+    )
+
+    if (all.some((e) => e.isLoading)) return <div>"Loading..."</div>
+    if (all.some((e) => e.error)) return <div>"Error..."</div>
+
     return (
         <StyledHome>
-            {titles.map((title, index) => (
-                <Carousel key={index} title={title} index={index} />
+            {all.map((i, index) => (
+                <Carousel key={index} title={titles[index]} data={i.data.results} index={index} />
             ))}
         </StyledHome>
     )

719eabcf16e71893c1f8c0e8b1108120c424b777
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-27 16:24:34 +0630

    feat(ui-thumb): make thumb clickable
    Need to set proper thumb component with styles.
    thumb.tsx : wrap with Link component, only wrapping text so the whole
                thumb is not clickable yet

--- libs/shared/ui/src/lib/thumb/thumb.tsx
+++ libs/shared/ui/src/lib/thumb/thumb.tsx
@@ -1,25 +1,37 @@
+import { Link } from "react-router-dom"
+
 interface ThumbProps {
     title: string
-    item: { name: string; author?: string; olid?: string }
+    item: { _id?: string; name: string; author?: string; olid?: string }
 }
 
 const Thumb = ({ title, item }: ThumbProps) => {
-    if (title.toLowerCase() === "books") {
-        return item.olid !== "unknown" ? (
-            <img
-                src={`https://covers.openlibrary.org/b/olid/${item.olid}-M.jpg`}
-                alt={`${item.name}`}
-                className="listItemImg"
-            />
+    if (title.toLowerCase().includes("books")) {
+        return item.olid ? (
+            <div className="listItem">
+                <Link to={`/books/${item._id}`}>
+                    <img
+                        src={`https://covers.openlibrary.org/b/olid/${item.olid}-M.jpg`}
+                        alt={`${item.name}`}
+                        className="listItemImg"
+                    />
+                </Link>
+            </div>
         ) : (
             <div className="listItem">
-                {item.name}
-                {"\nAuthor: "}
-                {item.author}
+                <Link to={`/books/${item._id}`}>
+                    {item.name}
+                    {"\nAuthor: "}
+                    {item.author}
+                </Link>
             </div>
         )
     }
 
-    return <div className="listItem">{item.name}</div>
+    return (
+        <div className="listItem">
+            <Link to={`/books/${item._id}`}>{item.name}</Link>
+        </div>
+    )
 }
 export default Thumb

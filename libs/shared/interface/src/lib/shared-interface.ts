df49961bb31d7f06b9af0f2d1097a6cd2f4c3827
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-01-30 16:41:49 +0630

    perf(interface): add new shared interface library
    libs/api-interfaces   : remove library
    libs/shared/interface : add as shared interface library

---
+++ libs/shared/interface/src/lib/shared-interface.ts
@@ -0,0 +1,74 @@
+export interface Book {
+    _id: string
+    name: string
+    author: string
+    isbn: number
+    olid: string
+    status: string
+    dateAdded: string
+    category: string
+}
+
+export interface Author {
+    _id: string
+    name: string
+    books: [{ _id: string; name: string }]
+}
+
+export interface Note {
+    _id: string
+    name: string
+    category: string
+}
+
+export interface Data {
+    totalResults: number
+    totalPages: number
+    page: number
+    limit: number
+    data: Book[] | Note[]
+}
+
+export interface ApiResponse {
+    total: number
+    total_pages: number
+    page: number
+    limit_per_page: number
+    results_in_page: number
+    results: Book[] | Author[] | Note[]
+}
+
+interface SingleBook extends Book {
+    similar: Book[]
+    notes: []
+    relatedNotes: Note[]
+}
+
+export interface BookResponse extends ApiResponse {
+    results: SingleBook[]
+}
+
+export interface Result {
+    total: [{ total: number }]
+    data: Book[] | Note[]
+}
+
+export interface QueryDB {
+    result: Result[]
+    page: number
+    limit: number
+}
+
+export interface User {
+    name: string
+    password: string
+    salt: string
+    role: string
+    createJWT: () => Promise<string>
+    validPassword: (password: string) => Promise<boolean>
+}
+
+export interface Category {
+    name: string
+    books: Array<string>
+}

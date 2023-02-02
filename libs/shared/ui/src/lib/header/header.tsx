225f905a259d129edbdd46820cac82f5c66a4679
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-02-02 16:24:23 +0630

    feat(ui-header): add search bar and side menu
    header.tsx                : show menu icon to open drawer on left if
                                page reach certain breakpoint, need to
                                refactor the drawer as separate component
    search-bar/search-bar.tsx : use material ui search

--- libs/shared/ui/src/lib/header/header.tsx
+++ libs/shared/ui/src/lib/header/header.tsx
@@ -1,4 +1,4 @@
-// import { SearchBar } from "./search-bar/search-bar"
+import { SearchBar } from "./search-bar/search-bar"
 
 import { useEffect, useState } from "react"
 import { Link as RouterLink, useLocation } from "react-router-dom"
@@ -9,12 +9,23 @@ import Tab from "@mui/material/Tab"
 import Tabs from "@mui/material/Tabs"
 import BookIcon from "@mui/icons-material/Book"
 
+import Drawer from "@mui/material/Drawer"
+import List from "@mui/material/List"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
+import useMediaQuery from "@mui/material/useMediaQuery"
+import useTheme from "@mui/material/styles/useTheme"
+
 /* eslint-disable-next-line */
-export interface SharedUiHeaderProps {}
+export interface SharedUiHeaderProps { }
 
 export const Header = () => {
     const [tab, setTab] = useState(1)
     const { pathname } = useLocation()
+    const isMatch = useMediaQuery(useTheme().breakpoints.down("md"))
 
     const changeTab = (e: React.SyntheticEvent, value: number) => {
         setTab(value)
@@ -31,19 +42,59 @@ export const Header = () => {
     return (
         <AppBar position="fixed" sx={{ background: "#1d2021", color: "#ebdbb2" }}>
             <Toolbar>
-                <Tabs textColor="inherit" value={tab} indicatorColor="primary" onChange={changeTab}>
-                    <Tab
-                        icon={<BookIcon sx={{ marginX: "1.2rem" }} fontSize="large" />}
-                        component={RouterLink}
-                        to="/"
-                    />
-                    <Tab label="Home" component={RouterLink} to="/" />
-                    <Tab label="Books" component={RouterLink} to="/books" />
-                    <Tab label="Notes" component={RouterLink} to="/notes" />
-                </Tabs>
-                <MenuBar items={["Authors", "Categories"]} />
+                {isMatch ? (
+                    <>
+                        <SideMenu />
+                        <Tab
+                            icon={<BookIcon sx={{ marginX: "1.2rem" }} fontSize="large" />}
+                            component={RouterLink}
+                            to="/"
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Tabs
+                            textColor="inherit"
+                            value={tab}
+                            indicatorColor="primary"
+                            onChange={changeTab}
+                        >
+                            <Tab
+                                icon={<BookIcon sx={{ marginX: "1.2rem" }} fontSize="large" />}
+                                component={RouterLink}
+                                to="/"
+                            />
+                            <Tab label="Home" component={RouterLink} to="/" />
+                            <Tab label="Books" component={RouterLink} to="/books" />
+                            <Tab label="Notes" component={RouterLink} to="/notes" />
+                        </Tabs>
+                        <MenuBar items={["Authors", "Categories"]} />
+                    </>
+                )}
+                <SearchBar />
             </Toolbar>
         </AppBar>
+    )
+}
+
+const SideMenu = () => {
+    const [openDrawer, setOpenDrawer] = useState(false)
+
+    return (
+        <>
+            <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+                <List>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <ListItemText primary="Hello" />
+                        </ListItemIcon>
+                    </ListItemButton>
+                </List>
+            </Drawer>
+            <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+                <MenuIcon color="secondary" />
+            </IconButton>
+        </>
     )
 }
 
--- libs/shared/ui/src/lib/header/search-bar/search-bar.tsx
+++ libs/shared/ui/src/lib/header/search-bar/search-bar.tsx
@@ -1,38 +1,61 @@
-import { StyledSearchBar } from "./search-bar.styles"
+import { styled, alpha } from "@mui/material/styles"
+import InputBase from "@mui/material/InputBase"
+import SearchIcon from "@mui/icons-material/Search"
 
 /* eslint-disable-next-line */
-export interface SearchBarProps {}
+export interface SearchBarProps { }
 
 export function SearchBar() {
     return (
-        <StyledSearchBar>
-            <ul>
-                <li>
-                    <select id="" name="">
-                        <SearchDropDownItem name="All" />
-                        <SearchDropDownItem name="Book" />
-                        <SearchDropDownItem name="Author" />
-                        <SearchDropDownItem name="Category" />
-                        <SearchDropDownItem name="Note" />
-                    </select>
-                </li>
-                <li>
-                    <input type="text" placeholder="Search" />
-                </li>
-            </ul>
-        </StyledSearchBar>
+        <Search>
+            <SearchIconWrapper>
+                <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
+        </Search>
     )
 }
 
-interface SearchDropDownItemProps {
-    name: string
-}
-const SearchDropDownItem = (props: SearchDropDownItemProps) => {
-    return (
-        <option className="search-dropdown-item" value="">
-            {props.name}
-        </option>
-    )
-}
+const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "auto",
+    [theme.breakpoints.up("sm")]: {
+        marginLeft: "auto",
+        width: "auto",
+    },
+}))
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            width: "12ch",
+            "&:focus": {
+                width: "20ch",
+            },
+        },
+    },
+}))
 
 export default SearchBar

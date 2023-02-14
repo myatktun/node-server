fb11f8b78733938ad8b81a419d0133f0d36c8cdf
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-02-14 11:45:01 +0630

    feat(admin): add edit for books
    app.tsx : add edit for books, change icon for each resource

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,13 +1,17 @@
 import { Admin, Resource } from "react-admin"
 import { dataProvider } from "@projectx/admin/provider"
-import { BookList, AuthorList, CategoryList, NoteList } from "@projectx/admin/ui"
+import { BookList, BookEdit, AuthorList, CategoryList, NoteList } from "@projectx/admin/ui"
+import BookIcon from "@mui/icons-material/Book"
+import MenuBookIcon from "@mui/icons-material/MenuBook"
+import PeopleIcon from "@mui/icons-material/People"
+import AutoAwesomeMotionIcon from "@mui/icons-material/AutoAwesomeMotion"
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="books" list={BookList} />
-        <Resource name="authors" list={AuthorList} />
-        <Resource name="categories" list={CategoryList} />
-        <Resource name="notes" list={NoteList} />
+        <Resource name="books" list={BookList} edit={BookEdit} icon={BookIcon} />
+        <Resource name="authors" list={AuthorList} icon={PeopleIcon} />
+        <Resource name="categories" list={CategoryList} icon={AutoAwesomeMotionIcon} />
+        <Resource name="notes" list={NoteList} icon={MenuBookIcon} />
     </Admin>
 )
 

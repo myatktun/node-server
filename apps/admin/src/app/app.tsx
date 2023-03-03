b4c9bcb84cc2fd07aae271b3b74e644c1b7a43b7
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-03-03 22:37:03 +0630

    feat(admin): add auth provider

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,5 +1,5 @@
 import { Admin, Resource } from "react-admin"
-import { dataProvider } from "@projectx/admin/provider"
+import { dataProvider, authProvider } from "@projectx/admin/provider"
 import { BookList, BookEdit, AuthorList, CategoryList, NoteList } from "@projectx/admin/ui"
 import BookIcon from "@mui/icons-material/Book"
 import MenuBookIcon from "@mui/icons-material/MenuBook"
@@ -7,7 +7,7 @@ import PeopleIcon from "@mui/icons-material/People"
 import AutoAwesomeMotionIcon from "@mui/icons-material/AutoAwesomeMotion"
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin authProvider={authProvider} dataProvider={dataProvider}>
         <Resource name="books" list={BookList} edit={BookEdit} icon={BookIcon} />
         <Resource name="authors" list={AuthorList} icon={PeopleIcon} />
         <Resource name="categories" list={CategoryList} icon={AutoAwesomeMotionIcon} />

8b97c323347a5f351a9ea6dca8423a11a2f81e6d
Myat Kyaw Tun <myatktun2k@gmail.com>
2023-08-04 11:21:33 +0630

    refactor(admin): change default theme to dark

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,4 +1,4 @@
-import { Admin, Resource } from "react-admin"
+import { Admin, Resource, defaultDarkTheme } from "react-admin"
 import { dataProvider, authProvider } from "@projectx/admin/provider"
 import { BookList, BookEdit, AuthorList, CategoryList, NoteList } from "@projectx/admin/ui"
 import BookIcon from "@mui/icons-material/Book"
@@ -7,7 +7,7 @@ import PeopleIcon from "@mui/icons-material/People"
 import AutoAwesomeMotionIcon from "@mui/icons-material/AutoAwesomeMotion"
 
 const App = () => (
-    <Admin authProvider={authProvider} dataProvider={dataProvider}>
+    <Admin authProvider={authProvider} dataProvider={dataProvider} theme={defaultDarkTheme}>
         <Resource name="books" list={BookList} edit={BookEdit} icon={BookIcon} />
         <Resource name="authors" list={AuthorList} icon={PeopleIcon} />
         <Resource name="categories" list={CategoryList} icon={AutoAwesomeMotionIcon} />
